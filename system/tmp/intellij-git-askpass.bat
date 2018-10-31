7f84d9ff18495f771b37cdc214263bdb8a73c388
gelbman20 <gelbman20@gmail.com>
2018-10-31 13:39:30 +0200

    ADD: config folders

---
+++ system/tmp/intellij-git-askpass.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:/Program Files/JetBrains/WebStorm 2018.1.4/jre64/bin/java" -cp "C:/Program Files/JetBrains/WebStorm 2018.1.4/plugins/git4idea/lib/git4idea-rt.jar;C:/Program Files/JetBrains/WebStorm 2018.1.4/lib/xmlrpc-2.0.1.jar;C:/Program Files/JetBrains/WebStorm 2018.1.4/lib/commons-codec-1.9.jar;C:/Program Files/JetBrains/WebStorm 2018.1.4/lib/util.jar" org.jetbrains.git4idea.http.GitAskPassApp %*
